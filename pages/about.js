ed92b94f406d23caf6965f7b0be286dcaf6838b5
coen <coen@flashpoint.nl>
2019-05-06 17:14:08 +0200

    test

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ class AboutPage extends Component {
       <main>
         <Header />
         <section>
-          <p>
+          <p>coen
             This is another page of the SSR examples, you accessed it{" "}
             <strong>{this.props.isServer ? "server" : "client"} side</strong>.
           </p>
